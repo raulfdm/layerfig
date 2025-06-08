168ef08f647e2becdb56da75f6cdc0fef518ec37
Raul Melo <melo.raulf@gmail.com>
2025-06-08 08:21:07 +0200

    replace vitest with bun test api

--- src/app-config-builder.test.ts
+++ src/app-config-builder.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it } from "bun:test";
 
 import { z } from "zod";
 import { AppConfigBuilder } from "./app-config-builder.ts";
--- vitest.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-	test: {
-		retry: process.env.CI ? 2 : 0,
-	},
-});
