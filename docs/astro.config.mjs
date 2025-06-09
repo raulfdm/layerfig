a7e6d80bfdaf9de63aaafe5d484020bf50a34fc2
Raul Melo <melo.raulf@gmail.com>
2025-06-09 07:59:33 +0200

    add files

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,26 +1,38 @@
+import starlight from "@astrojs/starlight";
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: "My Docs",
+			social: [
+				{
+					icon: "github",
+					label: "GitHub",
+					href: "https://github.com/withastro/starlight",
+				},
+			],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: "Guides",
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: "Example Guide", slug: "guides/example" },
 					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: "Reference",
+					autogenerate: { directory: "reference" },
 				},
 			],
 		}),
 	],
+
+	adapter: cloudflare({
+		imageService: "compile",
+	}),
 });
