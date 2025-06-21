c2bceeb0d3834c865e0d344bd18bd130c6f2c9ae
Raul Melo <melo.raulf@gmail.com>
2025-06-21 07:41:13 +0200

    WIP

--- docs/src/content/docs/guides/docker.md
+++ docs/src/content/docs/guides/docker.md
@@ -1,3 +1,7 @@
 ---
 title: Docker
 ---
+
+In a lot cases, we build our application using Docker so we can host it somewhere else.
+
+We can achieve that in various ways but maybe the most optimal one would be by using a technique called [Multi-stage builds](https://docs.docker.com/build/building/multi-stage/), which consist on having a couple of layers preparing the application and at the end, build an image only the production code of our app, here's an example:
---
+++ examples/docker/src/entry-server.ts
@@ -0,0 +1,21 @@
+import { config } from "./config";
+import typescriptLogo from "./typescript.svg";
+
+export function render(_url: string) {
+	const html = `
+    <div>
+      <a href="https://vite.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+        <img src="${typescriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+      </a>
+      <h1>Hello Vite!</h1>
+    </div>
+    <section>
+      <h2>Configuration</h2>
+      <pre><code>${JSON.stringify(config, null, 2)}</code></pre>
+    </section>
+  `;
+	return { html };
+}
