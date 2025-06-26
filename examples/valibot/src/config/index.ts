3d8be474addf4986c4c8613eaae6bbe4e7c2dd2b
Raul Melo <melo.raulf@gmail.com>
2025-06-26 06:14:53 +0200

    add valibot example

---
+++ examples/valibot/src/config/index.ts
@@ -0,0 +1,14 @@
+import { ConfigBuilder } from "@layerfig/config";
+import * as v from 'valibot';
+
+export const configSchema = v.object({
+	appURL: v.pipe(v.string(),v.url()),
+});
+
+
+export const config = new ConfigBuilder({
+  validate: finalConfig => v.parse(configSchema, finalConfig)
+})
+	.addSource("base.json")
+	.addSource("prod.json")
+	.build();
