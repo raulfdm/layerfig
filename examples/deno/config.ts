74388161a6c006c5cc430b685429c46a0296dfcf
Raul Melo <melo.raulf@gmail.com>
2025-06-29 16:16:16 +0200

    update all examples

--- examples/deno/config.ts
+++ examples/deno/config.ts
@@ -1,12 +1,13 @@
 import { ConfigBuilder } from "npm:@layerfig/config";
-import {z} from "npm:zod";
-
-const schema = z.object({
-  port: z.number(),
-});
 
 export const config = new ConfigBuilder({
-  validate: (fullConfig) => schema.parse(fullConfig),
+  validate: (fullConfig, z) => {
+    const schema = z.object({
+      port: z.number(),
+    });
+
+    return schema.parse(fullConfig);
+  },
 })
   .addSource("base.json")
   .addSource("live.json")
