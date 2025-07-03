bc367f5ea4fcf942d2af7c8ced7ba0ab49054b89
Raul Melo <melo.raulf@gmail.com>
2025-07-03 08:29:49 +0200

    WIP

--- packages/config/src/config-builder.ts
+++ packages/config/src/config-builder.ts
@@ -3,170 +3,192 @@ import { z as zod } from "zod/v4";
 import type { ConfigParser } from "./parser/define-config-parser";
 import { basicJsonParser } from "./parser/parser-json";
 import {
-	AnyObject,
-	EnvVarSourceOptions,
-	type PartialEnvVarSource,
+  AnyObject,
+  EnvVarSourceOptions,
+  type PartialEnvVarSource,
 } from "./types";
 import { merge, readIfExist, set } from "./utils";
 
 const APP_ROOT_PATH = process.cwd();
 
 export interface ConfigBuilderOptions<
-	T extends object = Record<string, unknown>,
+  T extends object = Record<string, unknown>
 > {
-	/**
-	 * A function to validate the configuration object.
-	 * @param config - The configuration object to be validated
-	 * @param z - The zod 4 instance
-	 */
-	validate: (config: Record<string, unknown>, z: typeof zod) => T;
-	/**
-	 * The folder where the configuration files are located.
-	 * @default "./config"
-	 */
-	configFolder?: string;
-	/**
-	 * Load source from different source types
-	 */
-	parser?: ConfigParser;
-	/**
-	 * Prefix used to search for slotted values
-	 * @default "$"
-	 */
-	slotPrefix?: string;
+  /**
+   * A function to validate the configuration object.
+   * @param config - The configuration object to be validated
+   * @param z - The zod 4 instance
+   */
+  validate: (config: Record<string, unknown>, z: typeof zod) => T;
+  /**
+   * The folder where the configuration files are located.
+   * @default "./config"
+   */
+  configFolder?: string;
+  /**
+   * Load source from different source types
+   */
+  parser?: ConfigParser;
+  /**
+   * Prefix used to search for slotted values
+   * @default "$"
+   */
+  slotPrefix?: string;
 }
 
+type AcceptedSource = string | EnvVarConfig;
+
 export class ConfigBuilder<T extends object = Record<string, unknown>> {
-	#options: ConfigBuilderOptions<T>;
-	#partialConfig: AnyObject = {};
-	#appConfigFolderAbsolutePath: string;
+  #options: ConfigBuilderOptions<T>;
+  #partialConfig: AnyObject = {};
+  #appConfigFolderAbsolutePath: string;
+  #sources: AcceptedSource[] = [];
 
-	constructor(options: ConfigBuilderOptions<T>) {
-		this.#options = options;
-		this.#appConfigFolderAbsolutePath = path.join(
-			APP_ROOT_PATH,
-			this.#options.configFolder ?? "./config",
-		);
-	}
+  constructor(options: ConfigBuilderOptions<T>) {
+    this.#options = options;
+    this.#appConfigFolderAbsolutePath = path.join(
+      APP_ROOT_PATH,
+      this.#options.configFolder ?? "./config"
+    );
+  }
 
-	static createEnvVarSource(options: PartialEnvVarSource = {}) {
-		const parsedOptions = EnvVarSourceOptions.parse(options);
-		return new EnvVarConfig(parsedOptions);
-	}
+  static createEnvVarSource(options: PartialEnvVarSource = {}) {
+    const parsedOptions = EnvVarSourceOptions.parse(options);
+    return new EnvVarConfig(parsedOptions);
+  }
 
-	public build(): T {
-		return this.#options.validate(this.#partialConfig, zod);
-	}
+  public build(): T {
+    for (const source of this.#sources) {
+      this.#loadSource(source);
+    }
 
-	public addSource(source: string | EnvVarConfig): this {
-		if (source instanceof EnvVarConfig) {
-			this.#addEnvironmentVariables(source);
-		} else if (typeof source === "string") {
-			const fileName = source;
-			const filePath = path.resolve(
-				this.#appConfigFolderAbsolutePath,
-				fileName,
-			);
-			const fileExtension = this.#getFileExtension(filePath);
+    return this.#options.validate(this.#partialConfig, zod);
+  }
 
-			if (this.#parser.acceptsExtension(fileExtension) === false) {
-				throw new Error(
-					`".${fileExtension}" file is not supported by this parser. Accepted files are: "${this.#parser.acceptedFileExtensions.join(", ")}"`,
-				);
-			}
+  public addSource(source: AcceptedSource): this {
+    if (
+      source instanceof EnvVarConfig === false &&
+      typeof source !== "string"
+    ) {
+      throw new Error(`Source type of "${typeof source}" is not valid.`);
+    }
 
-			const fileContentResult = readIfExist(filePath);
+    this.#sources.push(source);
 
-			if (fileContentResult.ok === false) {
-				throw new Error(fileContentResult.error);
-			}
+    return this;
+  }
 
-			const finalContent = this.#replaceSlots(fileContentResult.data);
-			const parserResult = this.#parser.load(finalContent);
+  #loadSource(source: string | EnvVarConfig): this {
+    if (source instanceof EnvVarConfig) {
+      this.#addEnvironmentVariables(source);
+    } else if (typeof source === "string") {
+      const fileName = source;
+      const filePath = path.resolve(
+        this.#appConfigFolderAbsolutePath,
+        fileName
+      );
+      const fileExtension = this.#getFileExtension(filePath);
 
-			if (parserResult.ok) {
-				this.#partialConfig = merge({}, this.#partialConfig, parserResult.data);
-			} else {
-				throw parserResult.error;
-			}
-		} else {
-			throw new Error("Invalid source. Please provide a valid one.");
-		}
+      if (this.#parser.acceptsExtension(fileExtension) === false) {
+        throw new Error(
+          `".${fileExtension}" file is not supported by this parser. Accepted files are: "${this.#parser.acceptedFileExtensions.join(
+            ", "
+          )}"`
+        );
+      }
 
-		return this;
-	}
+      const fileContentResult = readIfExist(filePath);
 
-	#replaceSlots(fileContent: string) {
-		const slotPrefix = this.#options.slotPrefix || "$";
-		const regex = new RegExp(`\\${slotPrefix}\\w+`, "g");
+      if (fileContentResult.ok === false) {
+        throw new Error(fileContentResult.error);
+      }
 
-		const matches = fileContent.match(regex);
+      const finalContent = this.#replaceSlots(fileContentResult.data);
+      const parserResult = this.#parser.load(finalContent);
 
-		if (matches === null) {
-			return fileContent;
-		}
+      if (parserResult.ok) {
+        this.#partialConfig = merge({}, this.#partialConfig, parserResult.data);
+      } else {
+        throw parserResult.error;
+      }
+    } else {
+      throw new Error("Invalid source. Please provide a valid one.");
+    }
 
-		const uniqueSlots = new Set(matches);
+    return this;
+  }
 
-		let copy = fileContent;
+  #replaceSlots(fileContent: string) {
+    const slotPrefix = this.#options.slotPrefix || "$";
+    const regex = new RegExp(`\\${slotPrefix}\\w+`, "g");
 
-		for (const slot of uniqueSlots) {
-			const slotWithoutPrefix = slot.replace(slotPrefix, "");
-			const value = process.env[slotWithoutPrefix];
+    const matches = fileContent.match(regex);
 
-			// Do not replace if the variable is not there
-			if (typeof value === "string" && value !== "undefined") {
-				copy = copy.replaceAll(slot, value);
-			}
-		}
+    if (matches === null) {
+      return fileContent;
+    }
 
-		return copy;
-	}
+    const uniqueSlots = new Set(matches);
 
-	get #parser(): ConfigParser {
-		return this.#options.parser || basicJsonParser;
-	}
+    let copy = fileContent;
 
-	#addEnvironmentVariables(source: EnvVarConfig): void {
-		this.#partialConfig = merge({}, this.#partialConfig, source.getEnvVars());
-	}
+    for (const slot of uniqueSlots) {
+      const slotWithoutPrefix = slot.replace(slotPrefix, "");
+      const value = process.env[slotWithoutPrefix];
 
-	#getFileExtension(filePath: string): string {
-		return path.extname(filePath).slice(1);
-	}
+      // Do not replace if the variable is not there
+      if (typeof value === "string" && value !== "undefined") {
+        copy = copy.replaceAll(slot, value);
+      }
+    }
+
+    return copy;
+  }
+
+  get #parser(): ConfigParser {
+    return this.#options.parser || basicJsonParser;
+  }
+
+  #addEnvironmentVariables(source: EnvVarConfig): void {
+    this.#partialConfig = merge({}, this.#partialConfig, source.getEnvVars());
+  }
+
+  #getFileExtension(filePath: string): string {
+    return path.extname(filePath).slice(1);
+  }
 }
 
 class EnvVarConfig {
-	#separator: NonNullable<EnvVarSourceOptions["separator"]>;
-	#runtimeEnv: NonNullable<EnvVarSourceOptions["runtimeEnv"]>;
-	#prefixWithSeparator: string;
+  #separator: NonNullable<EnvVarSourceOptions["separator"]>;
+  #runtimeEnv: NonNullable<EnvVarSourceOptions["runtimeEnv"]>;
+  #prefixWithSeparator: string;
 
-	constructor({
-		prefix,
-		prefixSeparator,
-		separator,
-		runtimeEnv,
-	}: EnvVarSourceOptions) {
-		this.#prefixWithSeparator = `${prefix}${prefixSeparator}`;
+  constructor({
+    prefix,
+    prefixSeparator,
+    separator,
+    runtimeEnv,
+  }: EnvVarSourceOptions) {
+    this.#prefixWithSeparator = `${prefix}${prefixSeparator}`;
 
-		this.#separator = separator;
-		this.#runtimeEnv = runtimeEnv;
-	}
+    this.#separator = separator;
+    this.#runtimeEnv = runtimeEnv;
+  }
 
-	getEnvVars() {
-		const envKeys = Object.keys(process.env).filter((key) =>
-			key.startsWith(this.#prefixWithSeparator),
-		);
+  getEnvVars() {
+    const envKeys = Object.keys(process.env).filter((key) =>
+      key.startsWith(this.#prefixWithSeparator)
+    );
 
-		const tempObject = {} as Record<string, unknown>;
+    const tempObject = {} as Record<string, unknown>;
 
-		for (const envKey of envKeys) {
-			const keyWithoutPrefix = envKey.replace(this.#prefixWithSeparator, "");
-			const keyParts = keyWithoutPrefix.split(this.#separator).join(".");
+    for (const envKey of envKeys) {
+      const keyWithoutPrefix = envKey.replace(this.#prefixWithSeparator, "");
+      const keyParts = keyWithoutPrefix.split(this.#separator).join(".");
 
-			set(tempObject, keyParts, this.#runtimeEnv[envKey]);
-		}
+      set(tempObject, keyParts, this.#runtimeEnv[envKey]);
+    }
 
-		return AnyObject.parse(tempObject);
-	}
+    return AnyObject.parse(tempObject);
+  }
 }
