ff7c0adeb90bfe5c5312ba3946a9569f6c1b9899
Raul Melo <melo.raulf@gmail.com>
2025-07-05 06:08:32 +0100

    add environment variable resource (#76)

---
+++ packages/config/src/sources/env-var.test.ts
@@ -0,0 +1,75 @@
+import path from "node:path";
+import url from "node:url";
+
+import { describe, expect, it } from "vitest";
+import { basicJsonParser } from "../parser/parser-json";
+import { EnvironmentVariableSource } from "./env-var";
+import type { LoadSourceOptions } from "./type";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+const fixturePaths = path.resolve(__dirname, "../__fixtures__");
+
+const baseLoadSourceOptions: LoadSourceOptions = {
+	configFolderPath: fixturePaths,
+	parser: basicJsonParser,
+	runtimeEnv: process.env,
+	slotPrefix: "$",
+};
+
+describe("EnvironmentVariableSource", () => {
+	it("should load from environment variable", () => {
+		const envVarSource = new EnvironmentVariableSource();
+
+		expect(
+			envVarSource.loadSource({
+				...baseLoadSourceOptions,
+				runtimeEnv: {
+					APP_appURL: "https://my-site.com",
+					APP_api__port: "3000",
+				},
+			}),
+		).toEqual({
+			api: {
+				port: "3000",
+			},
+			appURL: "https://my-site.com",
+		});
+	});
+
+	it("should not add variables that does not match with prefix", () => {
+		const envVarSource = new EnvironmentVariableSource();
+
+		expect(
+			envVarSource.loadSource({
+				...baseLoadSourceOptions,
+				runtimeEnv: {
+					FOO: "bar",
+				},
+			}),
+		).toEqual({});
+	});
+
+	it("should consider custom prefix, prefixSeparator and prop separator", () => {
+		const envVarSource = new EnvironmentVariableSource({
+			prefix: "TEST",
+			prefixSeparator: "-",
+			separator: "_-_",
+		});
+
+		expect(
+			envVarSource.loadSource({
+				...baseLoadSourceOptions,
+				runtimeEnv: {
+					"TEST-appURL": "https://my-site.com",
+					"TEST-api_-_port": "5000",
+				},
+			}),
+		).toEqual({
+			appURL: "https://my-site.com",
+			api: {
+				port: "5000",
+			},
+		});
+	});
+});
