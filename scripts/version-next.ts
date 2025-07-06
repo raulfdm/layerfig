3e95d8f1e97e5630de1f59be83eb1680ca4ced15
Raul Melo <melo.raulf@gmail.com>
2025-07-06 12:06:38 +0200

    attempt only to update lock file

--- scripts/version-next.ts
+++ scripts/version-next.ts
@@ -1,16 +1,7 @@
 import { $ } from "bun";
 
-const nextVersionBranch = "changeset-release/next" as const;
-
 // By running versions PR, it'll create the versions, the branch and push all the changes
 await $`bun changeset version`;
 
 // ... now, we need to update the lock file so the pipelines and docs preview are deployable
-await $`git fetch origin`;
-await $`git checkout ${nextVersionBranch}`;
-await $`git pull origin ${nextVersionBranch} --rebase`;
-
 await $`bun install --lockfile-only`;
-await $`git add bun.lock`;
-await $`git commit -m "chore: update lock file"`;
-await $`git push origin ${nextVersionBranch}"`;
