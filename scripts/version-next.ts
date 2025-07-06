e70ff8650280ca727792a3881de0b1aef273b7ce
Raul Melo <melo.raulf@gmail.com>
2025-07-06 12:04:59 +0200

    refactor: improve lock file update process

--- scripts/version-next.ts
+++ scripts/version-next.ts
@@ -6,10 +6,10 @@ const nextVersionBranch = "changeset-release/next" as const;
 await $`bun changeset version`;
 
 // ... now, we need to update the lock file so the pipelines and docs preview are deployable
-await $`git checkout -b temp`;
-await $`git branch -D ${nextVersionBranch}`;
-await $`git pull origin ${nextVersionBranch}`;
+await $`git fetch origin`;
 await $`git checkout ${nextVersionBranch}`;
+await $`git pull origin ${nextVersionBranch} --rebase`;
+
 await $`bun install --lockfile-only`;
 await $`git add bun.lock`;
 await $`git commit -m "chore: update lock file"`;
