6c4a161890f524f9c3c46d44569d6900a4737afa
Raul Melo <melo.raulf@gmail.com>
2025-07-06 11:13:00 +0200

    remove branch constraint

--- scripts/version-next.ts
+++ scripts/version-next.ts
@@ -3,8 +3,5 @@ import { $ } from "bun";
 const nextVersionBranch = "changeset-release/next" as const;
 
 await $`bun changeset version`;
-
-if (process.env.GITHUB_REF === nextVersionBranch) {
-	await $`bun install`;
-	await $`git add bun.lock && git commit -m "chore: update lock file" && git push origin ${nextVersionBranch}`;
-}
+await $`bun install`;
+await $`git add bun.lock && git commit -m "chore: update lock file" && git push origin ${nextVersionBranch}`;
