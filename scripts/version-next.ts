7f47bf5f2f837939684d0a037708206363add04c
Raul Melo <melo.raulf@gmail.com>
2025-07-06 12:08:15 +0200

    remove script

--- scripts/version-next.ts
+++
@@ -1,7 +0,0 @@
-import { $ } from "bun";
-
-// By running versions PR, it'll create the versions, the branch and push all the changes
-await $`bun changeset version`;
-
-// ... now, we need to update the lock file so the pipelines and docs preview are deployable
-await $`bun install --lockfile-only`;
