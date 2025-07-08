cd6b54550d137161a2d0e29824954138c46563b4
Raul de Melo <melo.raulf@gmail.com>
2025-07-08 11:05:03 +0200

    try to force install

--- scripts/version-next.ts
+++ scripts/version-next.ts
@@ -3,4 +3,4 @@ import { $ } from "bun";
 // Update the version in package.json to the next version
 await $`bun changeset version`;
 // Update the lockfile to reflect the new version. Required to have the docs published correctly.
-await $`bun install --lockfile-only`;
+await $`bun install --lockfile-only --force`;
