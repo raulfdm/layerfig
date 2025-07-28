d02a32214b8657551416df3305f0188a5211cdec
Raul Melo <melo.raulf@gmail.com>
2025-07-28 17:07:58 +0200

    fix yaml example

--- examples/yaml/src/config.ts
+++ examples/yaml/src/config.ts
@@ -1,17 +1,16 @@
-import { ConfigBuilder } from "@layerfig/config";
-import { FileSource } from "@layerfig/config/sources/file";
+import { ConfigBuilder, FileSource } from "@layerfig/config";
 import yamlParser from "@layerfig/parser-yaml";
 
 export const config = new ConfigBuilder({
-	validate: (finalConfig, z) => {
-		const configSchema = z.object({
-			appURL: z.url(),
-		});
+  validate: (finalConfig, z) => {
+    const configSchema = z.object({
+      appURL: z.url(),
+    });
 
-		return configSchema.parse(finalConfig);
-	},
-	parser: yamlParser,
+    return configSchema.parse(finalConfig);
+  },
+  parser: yamlParser,
 })
-	.addSource(new FileSource("base.yaml"))
-	.addSource(new FileSource("prod.yaml"))
-	.build();
+  .addSource(new FileSource("base.yaml"))
+  .addSource(new FileSource("prod.yaml"))
+  .build();
