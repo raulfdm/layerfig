403e25a1ca9639afcb2114b99853a25805ae5be0
Raul Melo <melo.raulf@gmail.com>
2025-07-28 12:15:16 +0200

    fix json5 example

--- examples/json5/src/config.ts
+++ examples/json5/src/config.ts
@@ -1,5 +1,4 @@
-import { ConfigBuilder } from "@layerfig/config";
-import { FileSource } from "@layerfig/config/sources/file";
+import { ConfigBuilder, FileSource } from "@layerfig/config";
 import json5Parser from "@layerfig/parser-json5";
 
 export const config = new ConfigBuilder({
@@ -13,6 +12,4 @@ export const config = new ConfigBuilder({
 	parser: json5Parser,
 })
 	.addSource(new FileSource("base.jsonc"))
-	.addSource(new FileSource("local.json"))
-	.addSource(new FileSource("prod.json5"))
 	.build();
