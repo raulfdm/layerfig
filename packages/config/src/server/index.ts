29b8a08af5080bae4871fba38b4cf5b10535be08
Raul Melo <melo.raulf@gmail.com>
2025-07-28 07:32:11 +0200

    rename module test and fix imports

--- packages/config/src/server/index.ts
+++ packages/config/src/server/index.ts
@@ -1,2 +1,6 @@
+export { z } from "zod";
 export * from "../parser/config-parser";
+export * from "../sources/env-var";
+export * from "../sources/file";
+export * from "../sources/object";
 export * from "./config-builder";
