348a00ee4460a20cfaba9bb6b83946bd3b3a4705
Raul Melo <melo.raulf@gmail.com>
2025-07-28 07:27:44 +0200

    fix exports

--- packages/config/tsdown.config.ts
+++ packages/config/tsdown.config.ts
@@ -2,26 +2,26 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig([
 	{
-		entry: {
-			index: "./src/index.ts",
-			"sources/env/index": "./src/sources/env-var.ts",
-			"sources/file/index": "./src/sources/file.ts",
-			"sources/object/index": "./src/sources/object.ts",
-			zod: "./src/zod.ts",
-			"zod-mini": "./src/zod-mini.ts",
-		},
+		entry: { zod: "./src/zod.ts", "zod-mini": "./src/zod-mini.ts" },
 		dts: true,
+		format: ["cjs", "esm"],
+		platform: "neutral",
 		sourcemap: true,
+	},
+	{
+		entry: "./src/server/index.ts",
+		dts: true,
 		format: ["cjs", "esm"],
 		platform: "node",
+		sourcemap: true,
 	},
 	{
 		entry: {
 			client: "./src/client/index.ts",
 		},
 		dts: true,
-		sourcemap: true,
 		format: ["esm"],
 		platform: "browser",
+		sourcemap: true,
 	},
 ]);
