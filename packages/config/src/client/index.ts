05d42f79f7c23f0aaa5f7906d81dba353bb02b3e
Raul Melo <melo.raulf@gmail.com>
2025-08-11 07:17:59 +0200

    export ConfigBuilderOptions for client module

--- packages/config/src/client/index.ts
+++ packages/config/src/client/index.ts
@@ -2,3 +2,4 @@ export * from "../sources/env-var";
 export * from "../sources/object";
 export { z } from "../zod-mini";
 export * from "./config-builder";
+export type { ConfigBuilderOptions } from "./types";
